19a37df30fd57449622b0d4e87bf77519cfcc232
Dimanka96 <dimankaperera@gmail.com>
2023-02-11 13:39:17 +0530

    move to the frontend

--- frontend/src/dummyData.js
+++ frontend/src/dummyData.js
@@ -1,52 +1,52 @@
 export const Users = [
     {
         id:1,
-        profilePicture: "/assets/person/1.jpg",
+        profilePicture: "person/1.jpg",
         username: "James Smith",
     },
     {
         id:2,
-        profilePicture: "/assets/person/2.jpg",
+        profilePicture: "person/2.jpg",
         username: "Michael Smith",
     },
     {
         id:3,
-        profilePicture: "/assets/person/3.jpg",
+        profilePicture: "person/3.jpg",
         username: "Maria Garcia",
     },
     {
         id:4,
-        profilePicture: "/assets/person/4.jpg",
+        profilePicture: "person/4.jpg",
         username: "James Johnson",
     },
     {
         id:5,
-        profilePicture: "/assets/person/5.jpg",
+        profilePicture: "person/5.jpg",
         username: "Maria Martinez",
     },
     {
         id:6,
-        profilePicture: "/assets/person/6.jpg",
+        profilePicture: "person/6.jpg",
         username: "Mary Brown",
     },
     {
         id:7,
-        profilePicture: "/assets/person/7.jpg",
+        profilePicture: "person/7.jpg",
         username: "Sarah Davis",
     },
     {
         id:8,
-        profilePicture: "/assets/person/8.jpg",
+        profilePicture: "person/8.jpg",
         username: "Elizabeth Jones",
     },
     {
         id:9,
-        profilePicture: "/assets/person/9.jpg",
+        profilePicture: "person/9.jpg",
         username: "Martha Johnson",
     },
     {
         id:10,
-        profilePicture: "/assets/person/10.jpg",
+        profilePicture: "person/10.jpg",
         username: "Margaret Clark",
     },
 ];
@@ -55,7 +55,7 @@ export const Posts = [
     {
         id:1,
         desc:"Love for All,Heterd for None.",
-        photo:"/assets/post/1.jpg",
+        photo:"post/1.jpg",
         date:"5 min ago",
         userId:7,
         like:32,
@@ -64,7 +64,7 @@ export const Posts = [
     {
         id:2,
         desc:"Hello, Good Morning",
-        photo:"/assets/post/2.jpg",
+        photo:"post/2.jpg",
         date:"15 min ago",
         userId:2,
         like:3,
@@ -73,7 +73,7 @@ export const Posts = [
     {
         id:3,
         desc:"So Cute",
-        photo:"/assets/post/3.jpg",
+        photo:"post/3.jpg",
         date:"2 min ago",
         userId:3,
         like:7,
@@ -82,7 +82,7 @@ export const Posts = [
     {
         id:4,
         desc:"Super ",
-        photo:"/assets/post/4.jpg",
+        photo:"post/4.jpg",
         date:"1 hour ago",
         userId:4,
         like:41,
@@ -90,7 +90,7 @@ export const Posts = [
     },
     {
         id:5,
-        photo:"/assets/post/5.jpg",
+        photo:"post/5.jpg",
         date:"2 hours ago",
         userId:5,
         like:15,
@@ -99,7 +99,7 @@ export const Posts = [
     {
         id:6,
         desc:"Happy Birthday .......! ",
-        photo:"/assets/post/6.jpg",
+        photo:"post/6.jpg",
         date:"2 hours ago",
         userId:6,
         like:107,
